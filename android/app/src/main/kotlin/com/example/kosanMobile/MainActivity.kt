c92a16f419efcb0a4ec3a85b9736b9d62b483ba5
Ilham Agung Pratama <54496232+ilhamcapitalize@users.noreply.github.com>
2021-04-09 23:20:09 +0700

    kosan mobile

---
+++ android/app/src/main/kotlin/com/example/kosanMobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kosanMobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:kosanMobile/providers/space_provider.dart';
+import 'package:kosanMobile/theme.dart';
+import 'package:provider/provider.dart';
+import 'pages/splash_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => SpaceProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashPage(),
+      ),
+    );
+  }
+}
